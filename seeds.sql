b9d78f65a81e32364b066f19d393941c41a22b4a
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-29 15:38:44 -0600

    added seeds.sql with pre-filled roles, departments and employees

---
+++ seeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO department (id, name)
+VALUES 
+    ( 001, "Engineering"),
+    ( 002, "Finance"),
+    ( 003, "Legal"),
+    ( 004, "Sales");
+
+SELECT * FROM department;
+
+
+
+INSERT INTO role (id, title, salary)
+VALUES 
+    ( 001, "Sales Lead", 100000),
+    ( 002, "Salesperson", 80000),
+    ( 003, "Lead Engineer", 150000),
+    ( 004, "Software Engineer", 120000),
+    ( 005, "Account Manager", 160000),
+    ( 006, "Accountant", 125000),
+    ( 007, "Legal Team Lead", 250000),
+    ( 008, "Lawyer", 190000),
+
+
+SELECT * FROM role;
+
+
+
+INSERT INTO employee (id, first_name, last_name)
+VALUES 
+( 001, "Anthony", "Manzione")
+
+SELECT * FROM employee;
