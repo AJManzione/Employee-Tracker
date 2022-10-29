9cd6255255947bcfed250050cafa3bda05c5fd28
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-29 12:38:01 -0600

    first prompt and path's to functions connected

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 const inquirer = require('inquirer');
 const mysql2 = require('mysql2');
 const consoleTable = require('console.table');
+const { allowedNodeEnvironmentFlags } = require('process');
 
+menuPrompt()
+
+
+
+
+
+
+
+
+
+
+
+
+
+function menuPrompt() {
 inquirer.prompt (
     [
         {
-            type: 'input',
-            message: 'How are you?',
-            name: 'ok'
+            type: 'list',
+            message: "What would you like to do?",
+            name: 'welcome',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Roles', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
+            validate: (value) => { 
+                if(value){return true} 
+                else {return "Please make a selection"}
+            }
         }
     ]
 )
+
+.then ((answer) => {
+    let userAnswer = answer.welcome;
+
+    if(!userAnswer) {
+        return "You did not make a selection";
+    } else if (userAnswer == "View All Employees") {
+        viewAllEmployees();
+    } else if (userAnswer == "Add Employee") {
+        addEmployee();
+    } else if (userAnswer == "Update Employee Roles") {
+        updateEmployeeRole();
+    } else if (userAnswer == "View All Roles") {
+        viewAllRoles();
+    } else if (userAnswer == "Add Role") {
+        addRole();
+    } else if (userAnswer == "View All Departments") {
+        viewAllDepartments();
+    } else if (userAnswer == "Add Department") {
+        addDepartment();
+    }
+
+})
+}
